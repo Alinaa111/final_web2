59d637e9d4f561a2b96aa556cd9522cacf226ff6
alina <137275863+Alinaa111@users.noreply.github.com>
2026-02-08 13:57:28 +0500

    Changed logo

--- client2/src/pages/Checkout.js
+++ client2/src/pages/Checkout.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useCart } from '../contexts/CartContext';
-import { useAuth } from '../contexts/AuthContext';
 import { orderService } from '../services/api';
 import '../styles/Checkout.css';
 import SuccessModal from '../components/SuccessModal';
